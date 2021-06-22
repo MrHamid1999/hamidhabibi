e6ff4f94fea4d008dd68790e6b2c291014b0e378
hamid habibi <hamidhabibiaa@gmail.com>
2021-06-22 11:26:08 +0430

    primary files has been added

---
+++ JavaScirpt/slider.js
@@ -0,0 +1,8 @@
+window.addEventListener("load" , () => {
+    const left = document.querySelector(".left-part")
+    const name = document.querySelector(".blur-background h2")
+    const button = document.querySelector(".blur-background button")
+    left.style.width= "45%";
+    name.style.opacity = "1"
+    button.style.right = "0"
+})
