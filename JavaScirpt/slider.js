a85804bcd2e95ebc88b340922c2c66ac44bb988c
hamid habibi <hamidhabibiaa@gmail.com>
2021-07-02 16:40:07 +0430

    getting ready for putting project on github

--- JavaScirpt/slider.js
+++ JavaScirpt/slider.js
@@ -1,3 +1,5 @@
+let topMenu =  document.querySelector(".top-menu-wrapper")
+
 window.addEventListener("load" , () => {
     const left = document.querySelector(".left-part")
     const name = document.querySelector(".blur-background h2")
@@ -5,8 +7,21 @@ window.addEventListener("load" , () => {
     left.style.width= "45%";
     name.style.opacity = "1"
     button.style.right = "0"
+    window.addEventListener("scroll" , () => {
+        const firstPage = document.querySelector(".home-wrapper");
+        if (window.scrollY > firstPage.scrollHeight ) {
+            topMenu.classList.add("position")
+        } else {
+            topMenu.classList.remove("position")
+        }
+    })
 })
 let scroller = document.querySelector('.blur-background button');
 scroller.addEventListener("click" , () => {
   
+})
+let viewPageBtn = document.querySelector(".blur-background button");
+viewPageBtn.addEventListener("click" , () => {
+   topMenu.scrollIntoView({top:0 , behavior:"smooth"})
+    
 })
